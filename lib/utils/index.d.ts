f7c941d9b0aa8ccbcb27e4d6c28cd5fb8ee80c53
Surenjun <tlsurejun@163.com>
2022-04-01 10:34:29 +0800

    feat：去掉lib文件

--- lib/utils/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @desc 首字母大写
- */
-export declare const capitalizedTitleCase: (letter: string) => string;
-/**
- * @desc 中英文转换 调用百度翻译
- */
-export declare const translate: (word: string) => Promise<void>;
